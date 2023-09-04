e6164f0ba5fb49ef11ca804aa0992e50542db7de
xlh1001 <xiaolihai1001@gmail.com>
2023-09-04 17:26:49 +0800

    修改文件路径

---
+++ test/include/cfg_reward.hrl
@@ -0,0 +1,8 @@
+-record(cfg_reward, {
+	lev = 0 	%% 等级
+	,award = [] 	%% 奖励
+	,tips = "" 	%% 注释
+	,value1 = 0 	%% 公式测试
+	,value2 = 0 	%% 公式测试
+	,value3 = 0 	%% 公式测试
+}).
---
+++ test/include/cfg_reward2.hrl
@@ -0,0 +1,5 @@
+-record(cfg_reward2, {
+	lev = 0 	%% 等级
+	,award = [] 	%% 奖励
+	,tips = "" 	%% 注释
+}).
